bc142c66db7df655f036ea66c4484029cf325f54
Dan <dan.soft.studio@gmail.com>
2025-02-01 12:45:57 +0700

    update

--- TestText2/unit1.pas
+++ TestText2/unit1.pas
@@ -18,6 +18,9 @@ private
   var UniformPoints: TGLint;
   var UniformContours: TGLint;
   var UniformContourCount: TGLint;
+  var UniformXf: TGLint;
+  var UniformXp: TGLint;
+  var UniformSc: TGLint;
   var MyFont: TUTrueTypeFontShared;
 protected
   function RequestDebugContext: Boolean; override;
@@ -25,6 +28,7 @@ public
   procedure Initialize; override;
   procedure Finalize; override;
   procedure Tick; override;
+  procedure DrawGlyph(const Glyph: TUTrueTypeFont.TGlyph; const Pos: TUVec2);
 end;
 
 var Form1: TForm1;
@@ -83,6 +87,9 @@ begin
   UniformPoints := glGetUniformLocation(Shader, PGLchar(PAnsiChar('points')));
   UniformContours := glGetUniformLocation(Shader, PGLchar(PAnsiChar('contours')));
   UniformContourCount := glGetUniformLocation(Shader, PGLchar(PAnsiChar('contour_count')));
+  UniformXf := glGetUniformLocation(Shader, PGLchar(PAnsiChar('xf')));
+  UniformXp := glGetUniformLocation(Shader, PGLchar(PAnsiChar('xp')));
+  UniformSc := glGetUniformLocation(Shader, PGLchar(PAnsiChar('sc')));
   //UniformWVP := glGetUniformLocation(Shader, PGLchar(PAnsiChar('WVP')));
   //UniformTex0 := glGetUniformLocation(Shader, PGLchar(PAnsiChar('tex0')));
 end;
@@ -94,18 +101,28 @@ begin
 end;
 
 procedure TForm1.Tick;
-  var Points: TUVec2Array;
-  procedure AddPoint(const v: TUVec2);
-  begin
-    specialize UArrAppend<TUVec2>(Points, v);
-  end;
-  var Contours: TUVec2iArray;
   var W, V, P, WVP: TUMat;
-  var i, j, n, c: Int32;
   var GlyphId: UInt32;
   var Glyph: TUTrueTypeFont.TGlyph;
-  var p0, p1, p2: TUVec2;
+  var i, n: Int32;
+  var Pos: TUVec2;
+  const Str = 'Ж';//'!มกำล';
 begin
+  glViewport(0, 0, ClientWidth, ClientHeight);
+  glClearColor(0.4, 1, 0.8, 1);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  GlyphId := MyFont.Ptr.FindGlyph(3675);
+  Pos := TUVec2.Make(0, 0);
+  i := 1;
+  //while i <= Length(Str) do
+  begin
+    GlyphId := MyFont.Ptr.FindGlyph(UStrUTF8ToUTF32(Str, n, i));
+    Glyph := MyFont.Ptr.Glyphs[GlyphId];
+    DrawGlyph(Glyph, Pos);
+    Inc(i, n);
+    Pos.x := Pos.x + Glyph.Advance * 0.0025;
+  end;
   {
   W := TUMat.Scaling(1, TextureSize.y / TextureSize.x, 1);
   W := W * TUMat.RotationY(((GetTickCount64 mod 4000) / 4000) * UTwoPi);
@@ -113,28 +130,28 @@ begin
   P := TUMat.Proj(UPi * 0.5, ClientWidth / ClientHeight, 0.1, 100);
   WVP := W * V * P;
   //}
+end;
 
-  GlyphId := MyFont.Ptr.FindGlyph(3675);//UStrUTF8ToUTF32('Ж', i));
-  Glyph := MyFont.Ptr.Glyphs[GlyphId];
-  {for i := 0 to High(Glyph.Contours) do
+procedure TForm1.DrawGlyph(const Glyph: TUTrueTypeFont.TGlyph; const Pos: TUVec2);
+  var Points: TUVec2Array;
+  procedure AddPoint(const v: TUVec2);
   begin
-    for j := 0 to High(Glyph.Contours[i]) do
-    begin
-      if not Glyph.Contours[i][j].OnCurve then Continue;
-      p0 := (Glyph.Contours[i][j].Pos - Glyph.Bounds.Min) / Glyph.Bounds.Size;
-      n := (j + 1) mod Length(Glyph.Contours[i]);
-      p1 := (Glyph.Contours[i][n].Pos - Glyph.Bounds.Min) / Glyph.Bounds.Size;
-      if Glyph.Contours[i][n].OnCurve then
-      begin
-        AddLine(p0, p1);
-        Continue;
-      end;
-      n := (j + 2) mod Length(Glyph.Contours[i]);
-      p2 := (Glyph.Contours[i][n].Pos - Glyph.Bounds.Min) / Glyph.Bounds.Size;
-      AddCurve(p0, p1, p2);
-    end;
-  end;}
-
+    specialize UArrAppend<TUVec2>(Points, v);
+  end;
+  var Contours: TUVec2iArray;
+  var i, j, n, c: Int32;
+  var p0, p1, p2: TUVec2;
+  var GlyphSize: TUFloat;
+  var Xf: TUMat2;
+  var Xp: TUVec4;
+  var Sc: TUVec4;
+  var MaxRatio: TUFloat;
+  var Aspect: TUVec2;
+  var Scale: TUFloat;
+  const Area = 1 / 1;
+begin
+  GlyphSize := UMax(Glyph.Bounds.Size.x, Glyph.Bounds.Size.y);
+  Scale := 1.9 * Area;
   Contours := nil;
   for i := 0 to High(Glyph.Contours) do
   begin
@@ -147,10 +164,10 @@ begin
       n := j mod Length(Glyph.Contours[i]);
       if not Glyph.Contours[i][n].OnCurve then Continue;
       Inc(c);
-      p0 := (Glyph.Contours[i][n].Pos - Glyph.Bounds.Min) / Glyph.Bounds.Size;
+      p0 := (Glyph.Contours[i][n].Pos - Glyph.Bounds.Min) / GlyphSize;
       AddPoint(p0);
       n := (j + 1) mod Length(Glyph.Contours[i]);
-      p1 := (Glyph.Contours[i][n].Pos - Glyph.Bounds.Min) / Glyph.Bounds.Size;
+      p1 := (Glyph.Contours[i][n].Pos - Glyph.Bounds.Min) / GlyphSize;
       if Glyph.Contours[i][n].OnCurve then
       begin
         AddPoint((p0 + p1) * 0.5);
@@ -160,20 +177,23 @@ begin
         AddPoint(p1);
       end;
     end;
-    p0 := (Glyph.Contours[i][0].Pos - Glyph.Bounds.Min) / Glyph.Bounds.Size;
+    p0 := (Glyph.Contours[i][0].Pos - Glyph.Bounds.Min) / GlyphSize;
     AddPoint(p0);
     Contours[High(Contours)][1] := c;
   end;
-
-  //SetLength(Points, 2);
-  //Points[0] := [0.1, 0.5];
-  //Points[1] := [0.9, 0.5];
-  //Lines[0] := [0, Length(Points)];
-
-  glViewport(0, 0, ClientWidth, ClientHeight);
-  glClearColor(0.4, 1, 0.8, 1);
-  glClear(GL_COLOR_BUFFER_BIT);
-
+  MaxRatio := 1 / UMax(ClientWidth, ClientHeight);
+  Aspect := TUVec2.Make(ClientWidth * MaxRatio, ClientHeight * MaxRatio);
+  Xp := TUVec4.Make(
+    Pos.x, Pos.y,
+    Glyph.Bounds.Size.x / GlyphSize,
+    Glyph.Bounds.Size.y / GlyphSize
+  );
+  Xf := TUMat2.Make(
+    Xp.z * Scale * Aspect.y, 0, 0, Xp.w * Scale * Aspect.x
+  );
+  Sc := TUVec4.Make(
+    1 / 10, 1 / 10, 0, 0
+  );
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   glBindVertexArray(VertexArray);
@@ -182,6 +202,9 @@ begin
   glUniform2fv(UniformPoints, Length(Points), @Points[0]);
   glUniform2iv(UniformContours, Length(Contours), @Contours[0]);
   glUniform1i(UniformContourCount, Length(Contours));
+  glUniformMatrix2fv(UniformXf, 1, GL_FALSE, @Xf);
+  glUniform4fv(UniformXp, 1, @Xp);
+  glUniform4fv(UniformSc, 1, @Sc);
 
   glDrawArrays(GL_TRIANGLES, 0, 6);
 end;
